88fbe30eaee7069e8c14abf3cd77a97bca1a9a7f
Ibragim <i_gatiyatullin@kbtu.kz>
2025-09-09 21:35:29 +0500

    stack

--- lab1/b.cpp
+++ lab1/b.cpp
@@ -3,34 +3,32 @@
 #include <vector>
 using namespace std;
 
-int main(){
-
+int main() {
     int n;
     cin >> n;
 
     vector<int> v(n), ans(n);
-
     stack<int> st;
 
-    for(int i=0; i<n; i++){
-        int a;
-        cin >> a;
-        v[i]=a;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
     }
 
-
-    for(int i=0; i<n; i++){
-        while(!st.empty() && v[st.top()] >= v[i]){
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && v[st.top()] > v[i]) {
             st.pop();
         }
 
-        if(st.empty()) ans[i]=-1;
+        if (st.empty()) ans[i] = -1;
         else ans[i] = v[st.top()];
+
         st.push(i);
     }
 
-    for(int i=0; i<n; i++){
-        cout << ans[i] << ' ';
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
