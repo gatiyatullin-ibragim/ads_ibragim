51b9a3cae324b8228666ef770c01c211373d084d
Ibragim <i_gatiyatullin@kbtu.kz>
2025-09-10 20:22:15 +0500

    balanced sequence of letters

---
+++ lab1/d.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    stack<char> st;
+    for(int i=0; i<s.length(); ++i){
+        if(!st.empty() && st.top() == s[i]){
+            st.pop();
+        }else
+        {
+            st.push(s[i]);
+        }    
+    }
+
+    if(!st.empty()){
+        cout << "NO";
+    }else{
+        cout << "YES";
+    }
+    return 0;
+}
