cf2adca196cff0f64298d89660a5a2b04bc185b9
Ibragim <i_gatiyatullin@kbtu.kz>
2025-09-13 09:12:01 +0500

    just prime

---
+++ lab1/h.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isprime(int num){
+    if(num <= 1) return false;
+    for(int i=2; i*i <= num; ++i){
+        if(num%i==0)return false;
+    }
+    return true;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    if(isprime(n)) cout << "Yes";
+    else cout << "No" << endl;
+
+    return 0;
+}
