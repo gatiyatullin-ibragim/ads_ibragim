405c639769b9b94b61323e17ca0a242af6d60db7
Ibragim <i_gatiyatullin@kbtu.kz>
2025-09-16 21:36:12 +0500

    dq calculator

---
+++ lab1/j.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    deque<int> dq;
+
+    while(true){
+        char a;
+        cin >> a;
+
+        if(a == '+'){
+            int num;
+            cin >> num;
+            dq.push_front(num);
+        }else if(a == '-'){
+            int num;
+            cin >> num;
+            dq.push_back(num);
+        }else if(a == '*'){
+            if(dq.empty()){
+                cout << "error\n";
+            }else {
+                int res =0;
+                int a1 = dq.front();
+                int a2 = dq.back();
+                res += a1 + a2;
+                cout << res << endl;
+            }
+        }else if (a == '!'){
+            break;
+        }
+    }
+
+
+    return 0;
+}
